e5c46bc5356cb1635e3ba4d7ccce001b37f2b327
udaymankena <udaymankena91@gmail.com>
2018-07-29 08:13:21 -0400

    Percolation 63/100

--- src/com/javayadu/week1/ass/Percolation.java
+++ src/com/javayadu/week1/ass/Percolation.java
@@ -1,21 +1,23 @@
 package com.javayadu.week1.ass;
 
-import java.util.Arrays;
-import java.lang.IllegalArgumentException;
-
 public class Percolation {
 	   
-	   int[][][] grid;
-	   int[][] grid_sz;
-	   int[][] grid_full;
-	   int no_of_open_sites = 0;
-	   int grid_length = 0;
+	   private final int[][][] grid;
+	   private final int[][] grid_sz;
+	   private final boolean[][] grid_full;
+	   private final boolean[][] grid_bot_con;
+	   private int no_of_open_sites = 0;
+	   private int grid_length = 0;
+	   private boolean percolates = false;
 	   public Percolation(int n) {
 		   // create n-by-n grid, with all sites blocked
+		   if(n <= 0)
+			   throw new java.lang.IllegalArgumentException();
 		   grid_length = n;
 		   grid = new int[n][n][2];
 		   grid_sz = new int[n][n];
-		   grid_full = new int[n][n];
+		   grid_full = new boolean[n][n];
+		   grid_bot_con = new boolean[n][n];
 		   
 		   for(int i=0; i<n; i++) {
 			   for(int j=0; j<n; j++) {
@@ -24,12 +26,12 @@ public class Percolation {
 				   grid[i][j][1] = -1;
 				   // initializing the size and full arrays
 				   grid_sz[i][j] = 1;
-				   grid_full[i][j] = -1;
+				   grid_full[i][j] = false;
 				  
 			   }
 		   }
 	   }
-	   public void open(int row, int col) throws java.lang.IllegalArgumentException{
+	   public void open(int row, int col) {
 		   // initial checks
 		   if(row < 1 || col<1 || row > grid_length || col > grid_length )
 			   throw new IllegalArgumentException();
@@ -37,8 +39,6 @@ public class Percolation {
 			   row--;
 			   col--;
 		   }
-		   
-		   
 		   // open site (row, col) if it is not open already
 		   if(!isOpen(row + 1,col + 1)){ // these methods are independent..can't just use them for this method.
 			   //open the site....point the root to itself
@@ -46,8 +46,15 @@ public class Percolation {
 			   grid[row][col][1] = col;
 			   no_of_open_sites++;
 			   // if the site is in top row, mark it as full
-			   if(row == 0)
-				   grid_full[row][col] = 1;
+			   if(row == 0) {
+				   grid_full[row][col] = true;
+				   if(grid_length == 1)
+					   percolates = true;
+			   }
+			// if the site is in bottom row, mark it as bottom connected
+			   if(row == grid_length - 1) {
+				   grid_bot_con[row][col] = true;
+			   }
 		   
 			   // Left right top bottom
 			   //left
@@ -77,7 +84,7 @@ public class Percolation {
 			   
 		   }
 	   }
-	   public boolean isOpen(int row, int col) throws java.lang.IllegalArgumentException{
+	   public boolean isOpen(int row, int col) {
 		// initial checks
 		   if(row < 1 || col<1 || row > grid_length || col > grid_length)
 			   throw new IllegalArgumentException();
@@ -90,7 +97,7 @@ public class Percolation {
 		   return grid[row][col][0] != -1;
 		   
 	   }
-	   public boolean isFull(int row, int col) throws java.lang.IllegalArgumentException {
+	   public boolean isFull(int row, int col) {
 		// initial checks
 		   if(row < 1 || col<1 || row > grid_length || col > grid_length )
 			   throw new IllegalArgumentException();
@@ -106,7 +113,25 @@ public class Percolation {
 		   int root_row = root_current[0];
 		   int root_col = root_current[1];
 		   
-		   return grid_full[root_row][root_col] == 1;
+		   return grid_full[root_row][root_col];
+	   }
+	   
+	   private boolean isbotCon(int row, int col) {
+		   if(row < 1 || col<1 || row > grid_length || col > grid_length )
+			   throw new IllegalArgumentException();
+		   else {
+			   row--;
+			   col--;
+		   }
+		   // is site (row, col) full?
+		   if(!isOpen(row+1,col+1))
+			   return false;
+		   
+		   int[] root_current = root(row,col);
+		   int root_row = root_current[0];
+		   int root_col = root_current[1];
+		   
+		   return grid_bot_con[root_row][root_col];
 	   }
 	   public int numberOfOpenSites() {
 		   // number of open sites
@@ -115,18 +140,19 @@ public class Percolation {
 	   public boolean percolates() {
 		   // does the system percolate?
 		   // if the last row is connected to the first row, system percolates
-		   for(int i=0; i<grid_length; i++) {
+		   /*for(int i=0; i<grid_length; i++) {
 			   if(isFull((grid_length-1)+1,i+1))
 				   return true;
 		   }
-		   return false;
+		   return false;*/
+		   return percolates;
 	   }
 	   
 	   private void connect(int r1, int c1, int r2, int c2) {
 		   int[] root_r1c1 = root(r1,c1);
 		   int[] root_r2c2 = root(r2,c2);
 		   
-		  if(!Arrays.equals(root_r1c1,root_r2c2)) {
+		  if(!arr_equals(root_r1c1,root_r2c2)) {
 			  if(grid_sz[root_r1c1[0]][root_r1c1[1]] > 
 			  grid_sz[root_r2c2[0]][root_r2c2[1]]) {
 				  //r1c1 is bigger tree
@@ -139,9 +165,18 @@ public class Percolation {
 				  // marking as full
 				  //if the smaller grid is full, convert the bigger to full
 				  // if the bigger is full, it'll remain full
-				  if(	grid_full[root_r2c2[0]][root_r2c2[1]] == 1 ) {
-					  grid_full[root_r1c1[0]][root_r1c1[1]] = 1;
+				  if(	grid_full[root_r2c2[0]][root_r2c2[1]]) {
+					  grid_full[root_r1c1[0]][root_r1c1[1]] = true;
 				  }
+				  // marking bottom connected
+				  if(grid_bot_con[root_r2c2[0]][root_r2c2[1]]) {
+					  grid_bot_con[root_r1c1[0]][root_r1c1[1]] = true;
+				  }
+				  // checking if this connection makes the system percolate
+				  // percolates if the bigger tree is both full and bottom connected
+				  if(grid_full[root_r1c1[0]][root_r1c1[1]] && 
+						  grid_bot_con[root_r1c1[0]][root_r1c1[1]])
+					  percolates = true;
 			  }
 			  else {
 				//r2c2 is bigger tree
@@ -154,21 +189,22 @@ public class Percolation {
 				  // marking as full
 				  //if the smaller grid is full, convert the bigger to full
 				  // if the bigger is full, it'll remain full
-				  if(	grid_full[root_r1c1[0]][root_r1c1[1]] == 1 ) {
-					  grid_full[root_r2c2[0]][root_r2c2[1]] = 1;
+				  if(	grid_full[root_r1c1[0]][root_r1c1[1]] ) {
+					  grid_bot_con[root_r2c2[0]][root_r2c2[1]] = true;
 				  }
+				// marking bottom connected
+				  if(grid_bot_con[root_r1c1[0]][root_r1c1[1]]) {
+					  grid_bot_con[root_r2c2[0]][root_r2c2[1]] = true;
+				  }
+				  // checking if this connection makes the system percolate
+				  // percolates if the bigger tree is both full and bottom connected
+				  if(grid_full[root_r2c2[0]][root_r2c2[1]] && 
+						  grid_bot_con[root_r2c2[0]][root_r2c2[1]])
+					  percolates = true;
 			  }
 
 		  }
 		   
-	   }
-	   
-	   private boolean isConnected(int r1, int c1, int r2, int c2) {
-		   return Arrays.equals(root(r1,c1),root(r2,c2));
-	   }
-	   
-	   private int count(int row, int col) {
-		   return grid_sz[row][col];
 	   }
 	   
 	   private int[] root(int row, int col) {
@@ -178,24 +214,49 @@ public class Percolation {
 		   return root(grid[grid[row][col][0]][grid[row][col][1]][0],
 				   grid[grid[row][col][0]][grid[row][col][1]][1]);
 	   }
+	   
+	   private boolean arr_equals(int[]a,int[]b) {
+		   // comparison for roots....works for equal lenth int arrays
+		   if(a != b)
+			   return false;
+		   for(int i=0; i<a.length; i++) {
+			   if(a[i] != b[i])
+				   return false;
+		   }
+		   return true;
+	   }
 
 	   public static void main(String[] args) {
 		   // test client (optional)
-		   Percolation p = new Percolation(3);
-		   p.open(1,1);
+		   Percolation p = new Percolation(18);
+		   for(int i=1; i<=17; i++)
+			   p.open(i, 1);
+		   System.out.println("percolates: " + p.percolates());
+		   /*p.open(1,1);
+		   System.out.println("1,1");
+		   System.out.println("isFull: " + p.isFull(1, 1)); 
+		   System.out.println("isbotcon: " + p.isbotCon(1, 1));
+		   System.out.println("percolates: " + p.percolates());
+		   p.open(2,2);		   
+		   System.out.println("2,2");
+		   System.out.println("isFull: " + p.isFull(2, 2)); 
+		   System.out.println("isbotcon: " + p.isbotCon(2, 2));
+		   System.out.println("percolates: " + p.percolates());
 		   p.open(2,1);
-		   p.open(2,3);
+		   System.out.println("2,1");
+		   System.out.println("isFull: " + p.isFull(2, 1)); 
+		   System.out.println("isbotcon: " + p.isbotCon(2, 1));
+		   System.out.println("percolates: " + p.percolates());
+		   /*p.open(3,1);
+		   System.out.println("3,1");
+		   System.out.println("isFull: " + p.isFull(3, 1)); 
+		   System.out.println("isbotcon: " + p.isbotCon(3, 1));
+		   p.open(2,1);
+		   System.out.println("2,1");
+		   System.out.println("isFull: " + p.isFull(2, 1)); 
+		   System.out.println("isbotcon: " + p.isbotCon(2, 1));
+		   */
 		   
-		   
-		   
-		   System.out.println(p.isFull(1, 1));
-		   System.out.println(p.isFull(2, 1));
-		   System.out.println(p.isFull(2, 3));
-
-		   p.open(1,3);
-		   System.out.println(p.isFull(1, 3));
-		   
-		   System.out.println(p.percolates());
 		   
 	   }
 	}
--- src/com/javayadu/week1/ass/PercolationStats.java
+++ src/com/javayadu/week1/ass/PercolationStats.java
@@ -1,10 +1,62 @@
 package com.javayadu.week1.ass;
-
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
+	private final double[] trial_thresh;
+	private final double CONFIDENCE_CONSTANT = 1.96;
+	   public PercolationStats(int n, int trials)  {
+		   // perform trials independent experiments on an n-by-n grid
+		   if(n <= 0 || trials <= 0)
+			   throw new java.lang.IllegalArgumentException();
+		   trial_thresh = new double[trials];
+		   double perc_threshold = 0.0;
+		   for(int trial=0;trial<trials;trial++) {
+			   Percolation perc = new Percolation(n);
+			   while(!perc.percolates()) {
+				   int a = StdRandom.uniform(1, n+1);
+				   int b = StdRandom.uniform(1, n+1);
+				   //System.out.println(a + "," + b);
+				   perc.open(a,b);
+			   }
+			   //System.out.println("perc.numberOfOpenSites():" + perc.numberOfOpenSites());
+			   perc_threshold = (double)perc.numberOfOpenSites()/(n*n);
+			   //System.out.println("threshold: " + perc_threshold);
+			   trial_thresh[trial] = perc_threshold;
+			   }
+	   }
+	   public double mean() {
+		   // sample mean of percolation threshold
+		   return StdStats.mean(trial_thresh);
+	   }
+	   public double stddev() {
+		   // sample standard deviation of percolation threshold
+		   /*double st_dev_sum = 0;
+		   double mean = mean();
+		   for(int i=0; i<trial_thresh.length; i++) {
+			  st_dev_sum +=  (mean-trial_thresh[i])*(mean-trial_thresh[i]);
+		   }
+		   return Math.sqrt(st_dev_sum/(trial_thresh.length - 1)); */
+		   return StdStats.stddev(trial_thresh);
+	   }
+	   public double confidenceLo() {
+		   // low  endpoint of 95% confidence interval
+		   double sqrt_trials = Math.sqrt(trial_thresh.length);
+		   return  mean() - (CONFIDENCE_CONSTANT * stddev())/sqrt_trials;
+	   }
+	   public double confidenceHi() {
+		   // high endpoint of 95% confidence interval
+		   double sqrt_trials = Math.sqrt(trial_thresh.length);
+		   return  mean() + (CONFIDENCE_CONSTANT * stddev())/sqrt_trials;
+	   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	   public static void main(String[] args) {
+		   // test client (described below)
+		   int n = Integer.parseInt(args[0]);
+		   int trials = Integer.parseInt(args[1]);
+		   
+		   PercolationStats ps = new PercolationStats(n, trials);
+		   System.out.println("mean                    = " + ps.mean());
+		   System.out.println("stddev                  = " + ps.stddev());
+		   System.out.println("95% confidence interval = " + "[" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]" );
+	   }
 	}
-
-}
